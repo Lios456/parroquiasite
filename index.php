959d0b48a11c299fc0a92dc45301360361d6db85
Lios456 <luis.jerez0075@utc.edu.ec>
2024-10-24 20:44:09 -0500

    Cambio de la primera section en index.php

--- index.php
+++ index.php
@@ -139,8 +139,8 @@
     <div class="row align-items-center g-0 bg-secondary">
       <div class="col-lg-6">
         <div class="m-4 p-4 m-lg-5 p-lg-5">
-          <h6 class="text-white"><span class="text-primary">#</span>1 Best HVAC Services</h6>
-          <h2 class="display-4 fw-bold text-white my-4">Air duct cleaning, AC cleaning & Home Maintainance</h2>
+          <h3 class="fw-bold h3 text-white">Diócesis de Latacunga</h3>
+          <h2 class="display-4 fw-bold text-white my-4">Parroquia Eclesiástica "Santísima Trinidad" La Laguna</h2>
           <ul class="info d-flex flex-wrap align-items-center list-unstyled">
             <li class="fw-medium text-white d-flex align-items-center me-4">
               <svg class="text-primary me-1" width="20" height="20">
@@ -164,7 +164,7 @@
         </div>
       </div>
       <div class="col-lg-6">
-        <img src="images/billboard.jpg" alt="img" class="img-fluid">
+        <img src="images\iglesia\17.jpg" alt="img" class="img-fluid">
       </div>
     </div>
   </section>
